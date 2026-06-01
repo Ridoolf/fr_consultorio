950038f03cb2b01bd398fe17ddae729a6dc262ea
Lucas Ridolfi <lucasg.ridolfi@gmail.com>
2026-05-31 22:44:08 -0300

    Config DB via dj-database-url

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'pacientes',
     'turnos',
     'caja',
-    
+    'dj-database-url',
 ]
 
 MIDDLEWARE = [
@@ -80,12 +80,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default='sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3'),
+        conn_max_age=600,
+        conn_health_checks=True,
+    )
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
@@ -131,3 +131,8 @@ CORS_ALLOWED_ORIGINS = [
 import os
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+import os
+from pathlib import Path
+import dj_database_url 
+
